c0ae0fcb6e711e654b194d2e4482ee088274fcc6
Darren Campbell <darren2802@gmail.com>
2020-02-29 17:18:27 -0700

    Adds dark sky service and test

---
+++ app/services/dark_sky_service.rb
@@ -0,0 +1,18 @@
+class DarkSkyService
+  def weather_info(coordinates)
+    lat = coordinates[:lat]
+    lng = coordinates[:lng]
+    
+    json = get_json(lat, lng)
+  end
+
+  private
+
+    def get_json(lat, lng)
+      url = "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{lng}"
+
+      response = Faraday.get(url) { |req| req.params['exclude'] = 'minutely' }
+
+      JSON.parse(response.body, symbolize_names: true)
+    end
+end
