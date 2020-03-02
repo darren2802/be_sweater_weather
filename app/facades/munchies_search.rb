9ad744d13bb87ce764e10792c768c34a1b6bacb0
Darren Campbell <darren2802@gmail.com>
2020-03-02 10:37:38 -0700

    Adds forecast to munchies endpoint

--- app/facades/munchies_search.rb
+++ app/facades/munchies_search.rb
@@ -3,19 +3,29 @@ class MunchiesSearch
     @start_loc = start_loc
     @end_loc = end_loc
     @food = food
+    @travel_seconds = 0
+    @arrival_time_unix = 0
   end
 
   def travel_time
     directions = GoogleMapsService.directions(@start_loc, @end_loc)
+    @travel_seconds = directions[:routes][0][:legs][0][:duration][:value]
+    directions[:routes][0][:legs][0][:duration][:text]
+  end
 
-    travel_time = directions[:routes][0][:legs][0][:duration][:text]
-    travel_seconds = directions[:routes][0][:legs][0][:duration][:value]
+  def destination_forecast
+    dest_geocode = GoogleMapsService.geocode(@end_loc)
+    coordinates = dest_geocode[:geometry][:location]
+    @arrival_time_unix = Time.now.to_i + @travel_seconds
+    weather_data = DarkSkyService.time_machine_forecast(coordinates, @arrival_time_unix)
+    weather_data[:currently][:summary]
+  end
 
-
-    travel_time
+  def restaurant
+    
   end
 
   def information
-    Munchie.new(@end_loc, travel_time)
+    Munchie.new(@end_loc, travel_time, destination_forecast)
   end
 end
--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -10,13 +10,24 @@ class DarkSkyService
     json = get_json(lat, lng)
   end
 
+  def self.time_machine_forecast(coordinates, future_time)
+    new.time_machine_forecast(coordinates, future_time)
+  end
+
+  def time_machine_forecast(coordinates, future_time)
+    lat = coordinates[:lat]
+    lng = coordinates[:lng]
+
+    json = get_json(lat, lng, future_time)
+  end
+
   private
 
-    def get_json(lat, lng)
-      url = "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{lng}"
+  def get_json(lat, lng, time)
+    url = "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{lng},#{time}"
 
-      response = Faraday.get(url) { |req| req.params['exclude'] = 'minutely' }
+    response = Faraday.get(url) { |req| req.params['exclude'] = 'minutely' }
 
-      JSON.parse(response.body, symbolize_names: true)
-    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
