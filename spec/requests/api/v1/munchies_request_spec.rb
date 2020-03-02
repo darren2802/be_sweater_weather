cd81f429f4b88b83f4ab48034e6ea71e36b80aa0
Darren Campbell <darren2802@gmail.com>
2020-03-02 11:56:29 -0700

    Adds vcr to munchies request spec

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Munchies API' do
   it 'can retrieve food and forecast information for a destination city as well
-      as travel time to that city' do
+      as travel time to that city', :vcr do
 
     get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
 
