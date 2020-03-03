63c351a68cd042bbb3cac369bfe6809e5d01689b
Darren Campbell <darren2802@gmail.com>
2020-03-03 11:59:06 -0700

    Adds road trip endpoint sad path

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::RoadTripsController < ApplicationController
     destination = params[:destination]
     api_key = params[:api_key]
 
-    return render json: { message: 'unauthorized' }, status: 401 unless User.find_by(api_key: api_key)
+    return render json: { message: 'invalid request' }, status: 401 unless User.find_by(api_key: api_key)
 
     road_trip_search = RoadTripSearch.new(origin, destination)
     render json: RoadTripSerializer.new(road_trip_search.information), status: 202
--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 describe 'User with api key can plan road trip', :vcr do
-  it 'returns travel time and destination forecast' do
-    # register a user to get an api key
+  before :each do
     post '/api/v1/users', :params => {
       "email": "whatever@example.com",
       "password": "password",
@@ -12,12 +11,14 @@ describe 'User with api key can plan road trip', :vcr do
     expect(response.status).to eq(201)
 
     result = JSON.parse(response.body, symbolize_names: true)
-    api_key = result[:data][:attributes][:api_key]
+    @api_key = result[:data][:attributes][:api_key]
+  end
 
+  it 'returns travel time and destination forecast if valid api key' do
     post '/api/v1/road_trip', :params => {
                                 "origin": "Denver,CO",
                                 "destination": "Pueblo,CO",
-                                "api_key": api_key
+                                "api_key": @api_key
                               }
 
     expect(response).to be_successful
@@ -30,5 +31,29 @@ describe 'User with api key can plan road trip', :vcr do
     expect(result[:data][:id]).to be_nil
     expect(result[:data][:attributes].keys).to eq([:origin, :destination, :travel_time, :arrival_forecast])
     expect(result[:data][:attributes][:arrival_forecast].keys).to eq([:temperature, :description])
+  end
+
+  it 'returns an error message if no api key' do
+    post '/api/v1/road_trip', :params => {
+                                "origin": "Denver,CO",
+                                "destination": "Pueblo,CO",
+                                "api_key": ''
+                              }
+
+    expect(response.status).to eq(401)
+    result = JSON.parse(response.body, symbolize_names: true)
+    expect(result).to eq({ message: "invalid request" })
+  end
+
+  it 'returns an error message if invalid api key' do
+    post '/api/v1/road_trip', :params => {
+                                "origin": "Denver,CO",
+                                "destination": "Pueblo,CO",
+                                "api_key": 'd3+QvCZ3zz0yNtvTAAbk5mSEpYuP'
+                              }
+
+    expect(response.status).to eq(401)
+    result = JSON.parse(response.body, symbolize_names: true)
+    expect(result).to eq({ message: "invalid request" })
   end
 end
